b344fc015eb0238039922864fac7532d296ce936
Luis Arango <learangof@outlook.com>
2022-07-15 07:29:20 +1200

    some extras

--- README.md
+++ README.md
@@ -13,3 +13,19 @@ Performance table (Constants)
 | Roundabout | 50% efficient | 75% efficient | 90% efficient
 | Stop Signs | 20% efficient | 30% efficient | 40% efficient
 | Traffic Lights | 90% efficient | 75% efficient | 30% efficient
+
+## Extensions
+- [x] The Council would like to be able to manipulate the CPM values for each four roads without recompiling every time. An interactive prompt would be helpful. You may choose to prompt for input values on stdin, or accept them as command-line parameters.
+- [x] You might want to use visual formatting to make it more readable and visually appealing, or implement a GUI interface using Qt. If your program is a command-line program, you could add support for a "/?" or "--help" or "--usage" command-line flag.
+- [x] You might want to implement a batch system that produces the result for a large number of CPM values, read from a CSV file on disk or stdin.
+- [ ] You might want to extend the system to support arbitrary traffic control methods.
+- [x] Roundabouts tend to be more efficient when one through-road is busy, and the other is
+quiet. So, if roads 1 and 3 have a high CPM relative to 2 and 4, then a bonus efficiency score
+of +10% might be earned for the roundabout control method.
+- [ ] You might have noticed that stop signs never get the best score. However, they are very
+cheap to make. Your program could allow the user to enter costs of each of the 3 control methods. You can then output a CPM-per-dollar value, indicating the number of cars the intersection can support, per dollar of construction cost. Roundabouts cost $100k, stop signs cost $40k and traffic lights cost $200k.
+- [x] Rather than three static states of throughput (low, medium, high), could these states be a continuous curve? Modify the algorithm to smoothly transition between these states. **I got the equations from**[reference](https://www.mathcelebrity.com/3ptquad.php?p1=10%2C0.9&p2=15%2C0.75&p3=20%2C0.50&pl=Calculate+Equation)
+- [ ] Add support for dual carriageway and/or pedestrian lanes. Can you allow a variable number of lanes in each direction?
+- [ ] Traffic lights can allow north-to-south and south-to-north car movement at the same time. What are all the other path combinations? What is the smallest covering set of paths that can be travelled simultaneously? Explore the maximum efficiency when each path combination has a different CPM rate.
+- [ ] Each of the 3 control methods has a different average latency before a new car is able to pass. At high throughput levels, traffic lights are more efficient, but a car might be able to
+enter an empty roundabout sooner. Invent some realistic latency numbers for each control method, and then investigate the latency-vs-throughput curve. What is the effect of cars “piling up” on average latency? Simulate this.
--- main.go
+++ main.go
@@ -1,102 +1,275 @@
 package main
 
 import (
+	"bufio"
+	"encoding/csv"
 	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-
-	Ncpm := readCPM("Road 1")
-	Ecpm := readCPM("Road 2")
-	Scpm := readCPM("Road 3")
-	Wcpm := readCPM("Road 4")
-
-	scores := map[string][]string{
-		"Road 1": efficiencyScore(Ncpm),
-		"Road 2": efficiencyScore(Ecpm),
-		"Road 3": efficiencyScore(Scpm),
-		"Road 4": efficiencyScore(Wcpm),
-	}
-
-	for key, value := range scores {
-		fmt.Printf("Suggested Traffic Control for %s is %s with a efficiency score of: %s\n", key, value[1], value[0])
-	}
-}
-func readCPM(road string) int {
-	var cpm int;
-	fmt.Printf("%s CPM:", road)
-	_, err := fmt.Scanf("%d",&cpm)
-	fmt.Printf("%s CPM:", err)
-	if err != nil {
-		for{
-			fmt.Printf("%s CPM:", road)
-			_, err := fmt.Scanf("%d",&cpm)
-			if err != nil {
+	fmt.Printf("\nWelcome!\n======================\nMenu\n======================\nPlease enter one of the following options:\n")
+	menu := "\n1: Suggestions based on table\n2: Suggestions based on equation\n3: Suggestions bulk (CSV)\n0: exit\n\n option: "
+	fmt.Print(menu)
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		opt, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			fmt.Printf(" Error, Please re enter your option: ")
+			continue
+		}
+		if opt == 0 {
+			break
+		}
+		switch opt {
+		case 1:
+			for {
+				CPM:= initCpmVariables()
+				score := suggestedControlByTable(CPM)
+				printResult(score,1)
+				if confirmation("\n======================\nDone\n======================\nWould you like to back to the menu?[y/n]: ") {
+					break
+				}
+			}
+		case 2:
+			CPM:= initCpmVariables()
+			score := suggestedControlByEquation(CPM)
+			printResult(score,1)
+			if confirmation("\n======================\nDone\n======================\nWould you like to back to the menu?[y/n]: ") {
 				break
 			}
+		case 3:
+			suggestionsFromCSV()
+			if confirmation("\n======================\nDone\n======================\nWould you like to back to the menu?[y/n]: ") {
+				break
+			}
+		default:
+			fmt.Printf("\n======================\nOption not Found\n======================\nPlease enter one of the following options:\n")
+			fmt.Print(menu)
+		}
+		fmt.Printf("\n======================\nMenu\n======================\nPlease enter one of the following options:\n")
+		fmt.Print(menu)
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Print("Error, Please re enter a integer")
+		os.Exit(1)
+	}
+	fmt.Printf("\n======================\nHASTA LA VISTA!\n======================\nThanks for using the program!\nHave a lovely day!\n")
+}
+func printResult(score []string,i int) {
+	fmt.Printf("Suggested Traffic Control for intersection %d CPM(%s) is %s with a efficiency score of: %s\n",i,score[2], score[1], score[0])
+}
+func confirmation(msg string) bool {
+	fmt.Print(msg)
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		if strings.EqualFold(scanner.Text(), "y") || strings.EqualFold(scanner.Text(), "yes") {
+			return true
+		} else if strings.EqualFold(scanner.Text(), "n") || strings.EqualFold(scanner.Text(), "no") {
+			return false
+		} else {
+			fmt.Print("Error, Please re enter : ")
 		}
 	}
-	return cpm
+	return false
 }
-func cpmLevel(cpm int) (map[string]float64, float64) {
+func initCpmVariables() map[string]float64 {
+	fmt.Printf("\n======================\nPlease enter the following Cars-Per-Minute (CPM) rates as integers > 0\n\n")
+	// Ncpm, Ecpm, Scpm, Wcpm := 11, 5, 21, 24
+	Ncpm := readCPM("North")
+	Ecpm := readCPM("East")
+	Scpm := readCPM("South")
+	Wcpm := readCPM("West")
 
-	HT := map[string]float64{
-		"Roundabout": 0.5,
-		"Stop Signs": 0.2,
-		"Traffic Lights": 0.9,
+	N, E, S, W := float64(Ncpm), float64(Ecpm), float64(Scpm), float64(Wcpm)
+	return map[string]float64{
+			"N":N,
+			"E":E,
+			"S":S,
+			"W":W,
+		}
+}
+func readCPM(road string) int {
+	fmt.Printf("%s CPM:", road)
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		cpm, err := strconv.Atoi(scanner.Text())
+		if err == nil {
+			fmt.Printf(" %s CPM is %d\n\n", road, cpm)
+			return cpm
+		} else {
+			fmt.Printf(" Error, Please re enter %s as integer > 0\n %s CPM:", road, road)
+		}
 	}
-	MT := map[string]float64{
-		"Roundabout": 0.75,
-		"Stop Signs": 0.3,
-		"Traffic Lights": 0.75,
+	if err := scanner.Err(); err != nil {
+		fmt.Print("Error, Please re enter a integer")
 	}
-	LT := map[string]float64{
-		"Roundabout": 0.9,
-		"Stop Signs": 0.4,
-		"Traffic Lights": 0.3,
-	}
+	return 0
+}
 
-	var cpmAvg float64
+func controlPerformanceTable(cpm float64) (map[string]float64) {
+	HT,MT,LT := initPerformanceControlConst()
+
 	var controlPerformance map[string]float64
 
 	switch {
-	case cpm < 10:
-		controlPerformance = LT
-		cpmAvg = 9.0
-	case cpm < 20:
-		controlPerformance = MT
-		cpmAvg = 19.0
-	default:
-		controlPerformance = HT
-		cpmAvg = 20.0
+		case cpm < 10:
+			controlPerformance = LT
+		case cpm < 20:
+			controlPerformance = MT
+		default:
+			controlPerformance = HT
 	}
-	return controlPerformance, cpmAvg
+	return controlPerformance
 }
-func efficiencyScore(cpm int)[]string{
+func efficiencyScore(CPM map[string]float64) float64{
+	var cpmScore float64 = 0.0
+	for _, value := range CPM {
+		weight := 0.8/getCpmState(value)
+		cpmScore += (value*weight)
+		fmt.Printf("CPM %f Weight %f Score %f CPMTotal %f \n\n",value,weight,(value*weight),cpmScore)
+	}
+	return cpmScore
+}
+func suggestedControlByTable(CPM map[string]float64) []string {
 	var scoreMax float64 = 0.0
 	var controlMethod string = ""
-	controlPerformance, cpmAvg := cpmLevel(cpm)
+	var cpmScore float64 = efficiencyScore(CPM)
+	controlPerformance := controlPerformanceTable(cpmScore)
+
+	bonus := false
+	if((getCpmState(CPM["N"]) > getCpmState(CPM["E"])) && (getCpmState(CPM["N"]) > getCpmState(CPM["W"])) && (getCpmState(CPM["S"]) > getCpmState(CPM["W"])) && (getCpmState(CPM["S"]) > getCpmState(CPM["E"]))) {
+		controlPerformance["Roundabout"] = controlPerformance["Roundabout"] + 0.1
+		bonus = true
+	}
+	if(getCpmState(cpmScore) == 2){
+		if cpmScore >= 14.5{
+			controlPerformance["Traffic Lights"] = controlPerformance["Traffic Lights"] + 0.1
+		}
+		if cpmScore < 14.5 {
+			controlPerformance["Roundabout"] = controlPerformance["Roundabout"] + 0.1
+
+		}
+		controlPerformance["Roundabout"] = controlPerformance["Roundabout"] + 0.1
+	}
 
 	for key, value := range controlPerformance {
-		//fmt.Printf("%v : %s -> %.2f\n",cpm, key, float64(cpm)*value/float64(cpmAvg))
-		if score := float64(cpm)*value/cpmAvg ; score > scoreMax {
-			if cpm >= 25 && key != "Roundabout"{
-				scoreMax = score
-				controlMethod = key
-				continue
-			}
-			if cpm >= 20 && key != "Traffic Lights"{
-				scoreMax = score
-				controlMethod = key
-				continue
-			}
-			scoreMax = score
+		if value > scoreMax {
+			scoreMax = value
 			controlMethod = key
 		}
 	}
 	if scoreMax > 1.0 {
 		scoreMax = 1.0
 	}
-	scoreString := []string{fmt.Sprintf("%.2f", (scoreMax*100))+"%", controlMethod}
+	if(controlMethod == "Roundabout" && bonus){
+		controlMethod = controlMethod + "(Bonus Applied)"
+	}
+	scoreString := []string{fmt.Sprintf("%.2f", (scoreMax*100)) + "%", controlMethod,fmt.Sprintf("%.2f", cpmScore)}
 	return scoreString
+}
+func initPerformanceControlConst()(map[string]float64,map[string]float64,map[string]float64){
+	HT := map[string]float64{
+		"Roundabout":     0.5,
+		"Stop Signs":     0.2,
+		"Traffic Lights": 0.9,
+	}
+	MT := map[string]float64{
+		"Roundabout":     0.75,
+		"Stop Signs":     0.3,
+		"Traffic Lights": 0.75,
+	}
+	LT := map[string]float64{
+		"Roundabout":     0.9,
+		"Stop Signs":     0.4,
+		"Traffic Lights": 0.3,
+	}
+	return HT,MT,LT
+}
+func getCpmState(cpm float64) float64{
+	state := 0.0
+	switch {
+		case cpm < 10:
+			state = 1
+		case cpm < 20:
+			state = 2
+		default:
+			state = 3
+	}
+	return state
+}
+func suggestedControlByEquation(CPM map[string]float64) []string {
+	var cpmScore float64 = efficiencyScore(CPM)
+	var scoreMax float64 = 0.0
+	var controlMethod string = ""
+
+	performanceControl := map[string]float64{
+		"Roundabout":    getRoundEquation(cpmScore),
+		"Stop Signs":     getStopEquation(cpmScore),
+		"Traffic Lights": getLightEquation(cpmScore),
+	}
+
+	bonus := false
+	if((getCpmState(CPM["N"]) > getCpmState(CPM["E"])) && (getCpmState(CPM["N"]) > getCpmState(CPM["W"])) && (getCpmState(CPM["S"]) > getCpmState(CPM["W"])) && (getCpmState(CPM["S"]) > getCpmState(CPM["E"]))) {
+		performanceControl["Roundabout"] = performanceControl["Roundabout"] + 0.1
+		bonus = true
+	}
+
+	for key, value := range performanceControl {
+		// fmt.Printf("\ncpm: %f key:%s score:%.2f \n",cpmScore, key, value)
+		if value > scoreMax {
+			scoreMax = value
+			controlMethod = key
+		}
+	}
+
+	if(controlMethod == "Roundabout" && bonus){
+		controlMethod = controlMethod + "(Bonus Applied)"
+	}
+	scoreString := []string{fmt.Sprintf("%.2f", (scoreMax*100)) + "%", controlMethod,fmt.Sprintf("%.2f", cpmScore)}
+	return scoreString
+}
+func getRoundEquation(cpm float64) float64{
+	return (-0.002 * math.Pow(cpm,2))+ (0.02*cpm) + 0.9
+}
+func getStopEquation(cpm float64) float64{
+	return (-0.02*cpm) + 0.6
+}
+func getLightEquation(cpm float64) float64{
+	return (-0.006 * math.Pow(cpm,2))+ (0.24*cpm) - 1.5
+}
+func suggestionsFromCSV() bool{
+	csvFile, err := os.Open("intersections.csv")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("\n======================\nSuccessfully Opened CSV file\n======================\n\n")
+	defer csvFile.Close()
+	csvLines, err := csv.NewReader(csvFile).ReadAll()
+    if err != nil {
+        fmt.Println(err)
+    }
+	for i, line := range csvLines {
+		N, E, S, W := string2Float(line[0]), string2Float(line[1]), string2Float(line[2]) , string2Float(line[3])
+		CPM := map[string]float64{
+				"N":N,
+				"E":E,
+				"S":S,
+				"W":W,
+		}
+		score := suggestedControlByEquation(CPM)
+		printResult(score,i+1)
+    }
+	return false
+}
+func string2Float(cpm string) float64 {
+	number, err := strconv.ParseFloat(cpm, 64)
+	if err != nil {
+		fmt.Printf("Error in value %s. It occur while reading the csv file: %v",cpm, err)
+		os.Exit(1)
+	}
+	return number
 }
